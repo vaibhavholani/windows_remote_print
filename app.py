a21e30ec7f29748d5db090c38187f75f296a1e68
Vaibhav Holani <vaibhavholani@gmail.con>
2023-09-15 17:59:13 -0400

    fixing basic errors

--- app.py
+++ app.py
@@ -18,16 +18,18 @@ def index():
             flash('No selected file')
             return redirect(request.url)
         if file:
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            # Using os.path.abspath to get the absolute path of the file
+            filepath = os.path.abspath(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
             file.save(filepath)
             print_file(filepath)
             flash('File printed successfully!')
     return render_template('index.html')
 
 def print_file(filepath):
-    # Send the print job
-    print("Printing file: " + filepath)
-    win32api.ShellExecute(0, "print", filepath, None, ".", 0)
-
+    if os.path.exists(filepath):
+        win32api.ShellExecute(0, "print", filepath, None, ".", 0)
+    else:
+        print(f"File not found: {filepath}")
+        
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
